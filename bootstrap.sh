697d142dfef71883ffdd666961ad2fa5241da9d0
Jacob Benny John <jacobben85@gmail.com>
2014-11-17 16:14:36 -0500

    Install mysql and mangooseim

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,16 @@ apt-get install -y node
 apt-get install -y npm
 apt-get install -y phantomjs
 
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
+sudo apt-get -y install mysql-server
+
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+dpkg -i erlang-solutions_1.0_all.deb
+
+apt-get update
+apt-get install -y mongooseim
+
 cd /home/vagrant/
 git clone https://github.com/jacobben85/code-everything.git
 
